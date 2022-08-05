f71bc43a6a1d1bce7c539c3bb304216b21e09e90
brunoecp <rm95315@fiap.com.br>
2022-08-05 09:51:41 -0300

    MEU REACT

---
+++ rm95315/aula01/src/app.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function app(){
+
+    const tag = <strong>"Olá React!!"</strong>
+
+    return(
+        <div>
+            <h1>{tag}</h1>,
+        </div>
+    )
+}
---
+++ rm95315/aula01/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./index.css"
+import ReactDom from 'react-dom'
+import app from "./app"
+
+ReactDom.render(
+    <app></app>,
+    document.getElementById("root")
+)
