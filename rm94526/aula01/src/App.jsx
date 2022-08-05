895905671e250d6b840c91ed4f54407961b105b7
caiogbarreira <rm94526@fiap.com.br>
2022-08-05 09:51:15 -0300

    * commit inicial

---
+++ rm94526/aula01/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function App(){
+
+    const tag = <strong>PUTS</strong>
+    return(
+        <div>
+            <h1>{tag}</h1>
+        </div>
+    )
+}
