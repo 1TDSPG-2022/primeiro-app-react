c3f5070c93221f9a5c9953e5b0dd529fa926ecc7
PedroLu1z <rm95189@fiap.com.br>
2022-08-05 09:53:44 -0300

    criado react e primeiro codigo html por meio dele

---
+++ rm95189/aula01/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function App(){
+    const tag = <strong>"Ola react!!"</strong>
+
+
+    return(
+        <div>
+            <h1>{tag}</h1>,
+        </div>
+    )
+}
