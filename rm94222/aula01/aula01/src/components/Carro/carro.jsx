41212f38ad68b16838b330b96ffce4d582e00245
lelemaia <rm94222@fiap.com.br>
2022-08-11 19:25:51 -0300

    jl,l

---
+++ rm94222/aula01/aula01/src/components/Carro/carro.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import carro1 from '../../img'
+
+export default function Carro() {
+
+    return(
+
+        <div>
+            <img src="(carro1)" alt="Carro bruto" />
+        </div>
+
+
+)
+}
