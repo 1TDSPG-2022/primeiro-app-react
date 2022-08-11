f0196c094351f51a6d29fbc99fc310506b3bfeb4
lelemaia <rm94222@fiap.com.br>
2022-08-11 18:54:55 -0300

    hv h

---
+++ rm94222/aula01/aula01/src/cabecalho/cabecalho.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Cabecalho() {
+
+
+return (
+
+    <header>
+        <h1>CABEÇALHO</h1>
+        <p>Componente de apresentação</p>
+    </header>
+
+
+)
+}
