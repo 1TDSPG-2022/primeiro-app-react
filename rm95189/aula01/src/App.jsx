5dbdee109482723e465f82476c4376acfaedf711
PedroLu1z <rm95189@fiap.com.br>
2022-08-12 09:53:54 -0300

    funçao soma

--- rm95189/aula01/src/App.jsx
+++ rm95189/aula01/src/App.jsx
@@ -4,14 +4,19 @@ import Carro from "./components/Carros";
 export default function App(){
     // const tag = <strong>"Ola react!!"</strong>
     //lista de automoveis
-
     const listaDeCarros = ['Fusca', 'Chevete', 'Brasilia', 'Uno', 'Ka']
+
+    //FUNÇÃO
+    const soma = (a,b)=>{
+        return a+b
+    }
 
     return(
         <div>
             <Cabecalho/>
             <Carro 
             listaDeCarros = {listaDeCarros}
+            soma={soma}
             />
         </div>
     )
--- rm95189/aula01/src/components/Carros.jsx
+++ rm95189/aula01/src/components/Carros.jsx
@@ -3,7 +3,11 @@ import carro from '../img/735_6.png'
 import './Carro.css'
  
 
-const Carros = (props) => {
+const Carros = (props, soma) => {
+
+    const executaFunc = (funcao) => {
+        
+    }
 
     return(
         <div className="lista-carros">
@@ -14,6 +18,12 @@ const Carros = (props) => {
                 <li>{carro}</li>
                 )}
             </ul>
+
+            <h2>Aplicação</h2>
+            <div>
+               <button onClick={props.soma(10,10)}></button>
+            
+            </div>
         </div>
     )
 }
