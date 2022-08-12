8d03ebe1bc222c073db2e0d498eb4705f5528652
lelemaia <rm94222@fiap.com.br>
2022-08-11 21:43:27 -0300

    ,

--- rm94222/aula01/aula01/src/components/Carro/carro.jsx
+++ rm94222/aula01/aula01/src/components/Carro/carro.jsx
@@ -7,7 +7,19 @@ export default function Carro() {
     return(
 
         <div>
-            <img src="(carro1)" alt="Carro bruto" width ="300px"/>
+            <figure>
+                <img src="(carro1)" alt="Carro bruto" width ="300px"/>
+                <figcaption>
+                    O bronco é bronco
+                </figcaption>
+            </figure>
+            <ol>
+                <li>Rnegade</li>
+                <li>Celta</li>
+                <li>Fusca</li>
+                <li>Opala</li>
+                <li>ix-35</li>
+            </ol>
         </div>
 
 
