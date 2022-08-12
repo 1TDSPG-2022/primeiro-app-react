2779494a67cb608958f611931ddf9999e2753a62
GuilhermeRocha0 <gui.rocha009@gmail.com>
2022-08-12 10:03:04 -0300

    feat: function soma

--- rm94771/aula01/src/App.jsx
+++ rm94771/aula01/src/App.jsx
@@ -1,13 +1,35 @@
 import React from 'react'
 import Cabecalho from './components/Cabecalho/Cabecalho'
 import Carro from './components/Carro/Carro'
+import carro_prof from './img/carro-prof.webp'
 
 export default function App() {
   const tag = <strong>Olá React!</strong>
+
+  // Informações para Veiculo
+  const altCarro = 'Carro de Familia'
+  const widthCarro = '200px'
+  const legendaCarro = 'Um carro para a vida inteira.'
+
+  // Lista de Automóvies
+  const listaDeCarros = ['Fusca', 'Chevete', 'Brasilia', 'Uno', 'Ka', 'Corsa']
+
+  // Função
+  const soma = (a, b) => {
+    return a + b
+  }
+
   return (
     <div>
       <Cabecalho />
-      <Carro />
+      <Carro
+        imgCarro={carro_prof}
+        altCarro={altCarro}
+        widthCarro={widthCarro}
+        legendaCarro={legendaCarro}
+        listaDeCarros={listaDeCarros}
+        soma={soma}
+      />
       <h1>{tag}</h1>
     </div>
   )
--- rm94771/aula01/src/components/Carro/Carro.jsx
+++ rm94771/aula01/src/components/Carro/Carro.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import carro1 from '../../img/carro1.jpg'
 
-export default function Carro() {
+export default function Carro(props) {
   return (
     <div>
       <figure>
@@ -15,6 +15,23 @@ export default function Carro() {
         <li>Opala</li>
         <li>IX-35</li>
       </ul>
+      <figure>
+        <img
+          src={props.imgCarro}
+          alt={props.altCarro}
+          width={props.widthCarro}
+        />
+        <figcaption>{props.legendaCarro}</figcaption>
+      </figure>
+      <h2>Lista de Carros</h2>
+      <ul>
+        {props.listaDeCarros.map((carro, i) => (
+          <li>{carro}</li>
+        ))}
+      </ul>
+
+      <h2>Aplicação</h2>
+      <div>{props.soma(10, 10) + 1}</div>
     </div>
   )
 }
