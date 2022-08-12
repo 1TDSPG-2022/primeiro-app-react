7ca8a9588a490c42dccab6f0c52e5c226375ea12
GuilhermeMenezesdaSilva <rm89384@fiap.com.br>
2022-08-12 09:56:08 -0300

    Exercicio carro criado e atualizado

--- RM89384/aula01/src/App.jsx
+++ RM89384/aula01/src/App.jsx
@@ -1,12 +1,33 @@
 import React from "react";
+import Carro from "./componentes/Carro/Carro"; //Todo import Exceto o React, significa que é filho de app
+import veraneio from './img/veraneio.webp'
 
 export default function App(){
 
-    const tag = <strong>"Olá React!!"</strong>
+    //Informação para veiculos
+    const altCarro = "Carro de familia"
+    const widthCarro = "200"
+    const legendaCarro = "Um carro para a vida inteira."
+    
+    //Lista de automóveis
+    const listaDeCarros = ["Fusca", "Chevete", "Brasilia", "Uno", "Ka"]
+
+    //Função
+    const soma = (a,b)=>{
+        return a+b
+    }
 
     return(
         <div>
-            <h1>{tag}</h1>
+            <Carro 
+                //Atributos do componente
+                imgCarro = {veraneio} 
+                altCarro = {altCarro}
+                widCarro = {widthCarro}
+                legendaCarro = {legendaCarro}
+                listaDeCarros = {listaDeCarros }
+                soma = {soma}
+            /> 
         </div>
     )
 }
---
+++ RM89384/aula01/src/componentes/Carro/Carro.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+export default function Carro(props){
+
+    let valor = 0 
+    const executaFuncao = (funcao)=>{
+        valor = valor + funcao
+        console.log(valor)
+    }
+
+    return(
+        <div>
+            <figure>
+                <img src={props.imgCarro} alt={props.altCarro} width={props.widCarro} />
+                <figcaption>{props.legendaCarro}</figcaption>
+            </figure>
+            <h2>Lista de Carros</h2>
+            <ul>
+                {
+                    props.listaDeCarros.map((carro,i)=>
+                    <li>{carro}</li>
+                )}
+            </ul>
+            <h2>Aplicação</h2>
+            <div>
+                <button onClick={executaFuncao(props.soma(10,10))}>SOMAR</button>
+                <span>Valor: {valor}</span>
+            </div>
+        </div>
+    )
+}
