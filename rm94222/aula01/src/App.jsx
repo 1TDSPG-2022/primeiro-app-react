d1336869532a8d557137bf689ddca4324d2f9169
lelemaia <rm94222@fiap.com.br>
2022-08-23 19:34:04 -0300

    n n

--- rm94222/aula01/src/App.jsx
+++ rm94222/aula01/src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import carro_prof  from './imgs/'
+import carro_prof  from './imgs/carro_prof.webp'
 import Lista from './components/Carro'
 
 export default function App() {
 
-  const carro = 'Matheus R'
+  const carro = ''
   const carros =['Fusca','HB20', 'Tucson', 'Peajout']
 
   const novoCarro =() => 'Ferrari'
@@ -12,7 +12,7 @@ export default function App() {
   return (
     <>
       <h1>Carros</h1>
-      <img src={carro_prof.webp} width='250px'></img>
+      <img src={carro_prof} width='250px'></img>
       <p>Carros: {carro}</p>
       <Lista carros = {carros} maisCarro={novoCarro}/>
     </>
--- rm94222/aula01/src/components/Carro.jsx
+++ rm94222/aula01/src/components/Carro.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
 export default function Carro(props){
+    
     return(
         <ul>
             <li>{props.carros[0]}</li>
             <li>{props.carros[1]}</li>
             <li>{props.carros[2]}</li>
             <li>{props.carros[3]}</li>
-            <li>{props.carros[4]}</li>
             <li>{props.maisCarro()}</li>
         </ul>
     )
